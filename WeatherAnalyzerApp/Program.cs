e2e327794197483449e01193290ec058a094d036
Ramandeep Nahar <ramandeep.s.nahar@gmail.com>
2026-03-21 15:27:51 -0400

    Added GetMax temp method

--- WeatherAnalyzerApp/Program.cs
+++ WeatherAnalyzerApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Weather Analyzer
+// This program analyzes a set of temperature values.
+// It finds the maximum, minimum, average temperature,
+// and counts how many days are considered hot.
+
+
+int[] temps = { 22, 28, 31, 35, 27 };
+
+static int GetMax(int[] temps)
+{
+    int maxTemp = temps[0];
+
+    foreach (int t in temps)
+    {
+        if ( t > maxTemp)
+        {
+            maxTemp = t;
+        }
+    }
+
+    return maxTemp;
+}
+
